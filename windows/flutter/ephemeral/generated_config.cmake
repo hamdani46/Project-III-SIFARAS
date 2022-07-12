ea664b4630d37afac613cb346425600be505fea2
hamdani46 <107379417+hamdani46@users.noreply.github.com>
2022-07-12 12:51:10 +0700

    update 4

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\DRIVE\\FLUTTER\\SIFARAS\\sifaras" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=E:\\DRIVE\\FLUTTER\\SIFARAS\\sifaras"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\DRIVE\\FLUTTER\\SIFARAS\\sifaras\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\DRIVE\\FLUTTER\\SIFARAS\\sifaras"
+  "FLUTTER_TARGET=E:\\DRIVE\\FLUTTER\\SIFARAS\\sifaras\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\DRIVE\\FLUTTER\\SIFARAS\\sifaras\\.dart_tool\\package_config.json"
+)
